e1cfcd320f347d1d2c4c9bcdd9c018f09ec02e44
dirav1 <dirav1@wal-mart.com>
2021-02-16 12:24:54 -0600

    updated search

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -1,47 +1,70 @@
-import './search.css'
+import "./search.css";
 
 import { Button, Container, Form } from "react-bootstrap";
+import { columns, emptySearchObject, noneDropDownObject } from './searchDefaults';
+import { useEffect, useState } from "react";
 
+import DropDown from "../../Controls/DropDown";
 import SearchDetails from "./SearchDetails";
 import axios from "axios";
-import { useState } from "react";
 
 const Search = () => {
   const [id, setId] = useState("");
   const [searchAll, setSearchAll] = useState(false);
+  const [selectedIdentification, setSelectedIdentification] = useState({id:"", description:""});
   const [searchResult, setSearchResult] = useState([
-    { firstname: "", middlename: "", lastname: "", id: "" },
+    emptySearchObject
   ]);
+  const [identifications, setIdentification] = useState([]);
+  useEffect(() => {
+    axios.get("/identification").then((response) => {
+      if (response) {
+        console.log(response.data);
+        console.log(noneDropDownObject);
+        console.log([...noneDropDownObject,...response.data]);
+        setIdentification([...noneDropDownObject, ...response.data]);
 
-  const getData = async (id)=> {
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getData = async (id, identificationId) => {
     var url = "http://localhost:5000/person";
     var result = [];
     if (id) url = `${url}/${id}`;
-    await axios.get(url).then((response) => {
-      console.log(response.data);
-      
-      result = Array.isArray(response.data) ? response.data : [response.data];
-    }).catch((ex)=> {console.log(ex);result = [];});
+    if((!id || id.trim().length <= 0) && identificationId) url = `${url}/?identificationId=${identificationId}`;
+    await axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+
+        result = Array.isArray(response.data) ? response.data : [response.data];
+      })
+      .catch((ex) => {
+        console.log(ex);
+        result = [];
+      });
     return result;
   };
-
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     var filteredResult = [];
-    console.log(`searchAll   :::  ${searchAll}`);
-    if(searchAll)
-    {
-    filteredResult = await getData();
-    }
-    else if (id && id.trim().length > 0 && !searchAll ) {
+    console.log(`selectedIdentification   :::  ${selectedIdentification.description}`);
+    console.log(selectedIdentification);
+    if (searchAll) {
+      filteredResult = await getData();
+    } else if (id && id.trim().length > 0 && !searchAll && selectedIdentification.id <= 0) {
       filteredResult = await getData(id);
-
+    } else if (!searchAll && selectedIdentification.id > 0) {
+      filteredResult = await getData('',selectedIdentification.id); 
     } else {
-      alert("Please Enter First Name to Search");
+      alert("Please Enter First Name or Identification to Search");
+      return;
     }
-    console.log('result is ');
-    console.log([filteredResult]);
+    console.log("result is ");
+    console.log(filteredResult);
     setSearchResult(...[filteredResult]);
   };
 
@@ -51,26 +74,13 @@ const Search = () => {
     setSearchResult([]);
     setSearchAll(false);
   };
-  const columns = [
-    {
-      Header: "Id",
-      accessor: "id",
-    },
-    {
-      Header: "First Name",
-      accessor: "firstname",
-    },
-    {
-      Header: "Middle Name",
-      accessor: "middlename",
-    },
-    {
-      Header: "Last Name",
-      accessor: "lastname",
-    },
-  ];
+  const handleDDChange = (e) => {
+    const {options, value} = e.target;
+    setSelectedIdentification({id:e.target.value,description:options[value].label});
+  };
+  
   return (
-    <Container className='block-border'>
+    <Container className="block-border">
       <Form.Group>
         <Form.Control
           placeholder="Enter Id"
@@ -78,13 +88,19 @@ const Search = () => {
           onChange={(e) => setId(e.target.value)}
           type="text"
         />
-        <Form.Check 
-    type="switch"
-    id="searchAllSwitch"
-    checked = {searchAll}
-    onChange={(e) => setSearchAll(e.currentTarget.checked)}
-    label="Search All"
-  />
+        <DropDown
+          options={identifications}
+          lblText="Identification Id"
+          name="identificationId"
+          onChange={handleDDChange}
+        ></DropDown>
+        <Form.Check
+          type="switch"
+          id="searchAllSwitch"
+          checked={searchAll}
+          onChange={(e) => setSearchAll(e.currentTarget.checked)}
+          label="Search All"
+        />
         <div>
           <Button variant="primary" onClick={handleSubmit} className="btn">
             Search
